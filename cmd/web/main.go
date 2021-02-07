59e2dee11043fb1ac1958a4a6742bb94ffe8cb2c
galamshar <myrzagalyev.galymzhan@gmail.com>
2021-02-07 23:29:51 +0600

    DOCKER SUPPORT

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,7 +23,7 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	pool, err := pgxpool.Connect(context.Background(), "user=postgres password=Zawer021 host=localhost port=5432 dbname=snippetbox sslmode=disable pool_max_conns=10")
+	pool, err := pgxpool.Connect(context.Background(), "user=postgres password=12345 host=0.0.0.0 port=5432 dbname=snippetbox sslmode=disable pool_max_conns=10")
 	if err != nil {
 		log.Fatalf("Unable to connection to database : %v\n", err)
 	}
